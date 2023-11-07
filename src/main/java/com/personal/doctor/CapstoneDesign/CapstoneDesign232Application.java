39d60c2155f69ef7b0686a4e4cec6f19e87b7a12
zestbear <zestbear90@gmail.com>
2023-11-07 16:01:15 +0900

    Application

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/CapstoneDesign232Application.java
@@ -0,0 +1,13 @@
+package com.personal.doctor.CapstoneDesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneDesign232Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneDesign232Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/doctor/CapstoneDesign/CapstoneDesign232ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.doctor.CapstoneDesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneDesign232ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
