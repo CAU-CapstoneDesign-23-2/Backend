e250b3a2ba9706fac9595e367e7d3019d7da0a61
zestbear <zestbear90@gmail.com>
2023-11-07 16:13:34 +0900

    중복된 User 에러 발생 Handler

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/common/UserAlreadyExistException.java
@@ -0,0 +1,9 @@
+package com.personal.doctor.CapstoneDesign.common;
+
+public class UserAlreadyExistException extends RuntimeException {
+
+    public UserAlreadyExistException(String message) {
+        super(message);
+    }
+
+}
