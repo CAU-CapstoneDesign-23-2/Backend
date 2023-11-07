d7fad0a205544b77850f19efe0502d12bebcd5da
zestbear <zestbear90@gmail.com>
2023-11-07 16:11:57 +0900

    User Dto: 사용자 정보 수정 및 추가

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/controller/dto/UserUpdateRequestDto.java
@@ -0,0 +1,16 @@
+package com.personal.doctor.CapstoneDesign.controller.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class UserUpdateRequestDto {
+    private String userName;
+    private Integer userAge;
+
+    @Builder
+    public UserUpdateRequestDto(String userName, Integer userAge) {
+        this.userName = userName;
+        this.userAge = userAge;
+    }
+}
