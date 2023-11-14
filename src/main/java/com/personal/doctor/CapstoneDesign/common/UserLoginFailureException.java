797a006af64dac396013cb7e9302791d76e8ee7b
zestbear <zestbear90@gmail.com>
2023-11-14 15:49:33 +0900

    사용자 Exception 작성

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/common/UserLoginFailureException.java
@@ -0,0 +1,9 @@
+package com.personal.doctor.CapstoneDesign.common;
+
+public class UserLoginFailureException extends RuntimeException {
+
+    public UserLoginFailureException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/personal/doctor/CapstoneDesign/common/UserNotExistException.java
@@ -0,0 +1,9 @@
+package com.personal.doctor.CapstoneDesign.common;
+
+public class UserNotExistException extends RuntimeException {
+
+    public UserNotExistException(String message) {
+        super(message);
+    }
+
+}
