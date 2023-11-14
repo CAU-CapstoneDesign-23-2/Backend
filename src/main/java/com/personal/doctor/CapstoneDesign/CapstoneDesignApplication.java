85ad72e9e1e8b8e9f19dabb53ed8dac073599623
zestbear <zestbear90@gmail.com>
2023-11-14 15:44:31 +0900

    Jpa 활성화

--- src/main/java/com/personal/doctor/CapstoneDesign/CapstoneDesignApplication.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/CapstoneDesignApplication.java
@@ -2,12 +2,14 @@ package com.personal.doctor.CapstoneDesign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class CapstoneDesign232Application {
+public class CapstoneDesignApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CapstoneDesign232Application.class, args);
+		SpringApplication.run(CapstoneDesignApplication.class, args);
 	}
 
 }
