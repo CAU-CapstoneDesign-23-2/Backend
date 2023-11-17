f524c85a9726c9fa32d01b3b504213d914bda2a3
zestbear <zestbear90@gmail.com>
2023-11-17 23:49:12 +0900

    게시물 Exception 추가

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/util/PostNOTExistException.java
@@ -0,0 +1,9 @@
+package com.personal.doctor.CapstoneDesign.util;
+
+public class PostNOTExistException extends RuntimeException {
+
+    public PostNOTExistException(String message) {
+        super(message);
+    }
+
+}
