ff7aef33bceaa1a67d58970678dacf9bc7cfd8ba
zestbear <zestbear90@gmail.com>
2023-11-17 19:05:52 +0900

    사용자 controller 주석 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/controller/UserController.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/controller/UserController.java
@@ -12,29 +12,31 @@ public class UserController {
 
     private final UserService userService;
 
+    // 사용자 회원 가입
     @PostMapping("/join")
     public Long join(@RequestBody UserJoinRequestDto requestDto) {
         return userService.join(requestDto);
     }
 
+    // 사용자 로그인
     @PostMapping("/login")
     public Long login(@RequestBody UserJoinRequestDto requestDto) {
         return userService.login(requestDto);
     }
 
+    // 사용자 정보 수정
     @PutMapping("/user/{id}")
     public Long updateUserInfo(@PathVariable Long id,
                                @RequestBody UserUpdateRequestDto requestDto) {
         return userService.update(id, requestDto);
     }
 
+    // 사용자 역할 수정
     @PutMapping("/user/role/{id}")
     public Long updateUserRole(@PathVariable Long id) {
         return userService.updateRole(id);
     }
 
     // TODO: UserDetails 저장 기능
-    // TODO: id -> stdId 변경
-    // TODO: 주석 달기
 
 }
