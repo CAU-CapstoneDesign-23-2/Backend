7eb93bb842ae69f8e58540d7f9f0f90b66be5b1f
zestbear <zestbear90@gmail.com>
2023-11-20 18:35:40 +0900

    사용자 service 회원가입 테스트 코드 작성

---
+++ src/test/java/com/personal/doctor/CapstoneDesign/user/UserServiceTest.java
@@ -0,0 +1,37 @@
+package com.personal.doctor.CapstoneDesign.user;
+
+import com.personal.doctor.CapstoneDesign.user.controller.dto.UserJoinRequestDto;
+import com.personal.doctor.CapstoneDesign.user.service.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @AfterEach
+    public void clean() {
+        userService.deleteAll();
+    }
+
+    @Test
+    public void saveUser() {
+        UserJoinRequestDto user = UserJoinRequestDto.builder()
+                .userID("ID")
+                .userPassword("PW")
+                .build();
+        Long userId = userService.join(user);
+
+        assertEquals(1, userId);
+    }
+
+}
