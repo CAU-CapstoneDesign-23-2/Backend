9b5cd6cb1cd4b6d3a69a5ccdc470e82d67a7de06
zestbear <zestbear90@gmail.com>
2023-11-22 19:21:53 +0900

    게시물 답변 권한 제한 Exception

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/util/exceptions/UserNotQualifiedException.java
@@ -0,0 +1,9 @@
+package com.personal.doctor.CapstoneDesign.util.exceptions;
+
+public class UserNotQualifiedException extends RuntimeException {
+
+    public UserNotQualifiedException(String message) {
+        super(message);
+    }
+
+}
