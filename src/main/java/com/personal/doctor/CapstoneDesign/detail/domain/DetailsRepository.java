411b4082800ff3af6933b29dd0bb221a5926c9db
zestbear <zestbear90@gmail.com>
2023-11-22 20:21:07 +0900

    findById 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/detail/domain/DetailsRepository.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/detail/domain/DetailsRepository.java
@@ -3,6 +3,11 @@ package com.personal.doctor.CapstoneDesign.detail.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DetailsRepository extends JpaRepository<Details, Long> {
+
+    Optional<Details> findById(Long id);
+
 }
