82ee49e864dbd76eb3f578c7e91c68fed9a5d3d6
zestbear <zestbear90@gmail.com>
2023-11-23 03:24:41 +0900

    사용자의 세부정보 찾기 기능 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/detail/domain/DetailsRepository.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/detail/domain/DetailsRepository.java
@@ -1,6 +1,8 @@
 package com.personal.doctor.CapstoneDesign.detail.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,8 @@ import java.util.Optional;
 public interface DetailsRepository extends JpaRepository<Details, Long> {
 
     Optional<Details> findById(Long id);
+
+    @Query("SELECT d FROM Details d WHERE d.users.id = :id")
+    Details findUserDetails(@Param("id") Long userId);
 
 }
