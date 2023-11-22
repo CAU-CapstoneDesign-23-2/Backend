3b97733f99d4f7e5778090450f1e4551332bf93f
zestbear <zestbear90@gmail.com>
2023-11-22 17:18:11 +0900

    NoArgsConstructor 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/user/controller/dto/UserUpdateRequestDto.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/user/controller/dto/UserUpdateRequestDto.java
@@ -2,7 +2,9 @@ package com.personal.doctor.CapstoneDesign.user.controller.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 public class UserUpdateRequestDto {
     private String userName;
