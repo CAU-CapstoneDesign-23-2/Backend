99b73a5d09817371d91ea1de6dcf27789820efd8
zestbear <zestbear90@gmail.com>
2023-11-23 22:59:10 +0900

    사용자 정보에 위치 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/user/controller/dto/UserUpdateRequestDto.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/user/controller/dto/UserUpdateRequestDto.java
@@ -8,9 +8,11 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UserUpdateRequestDto {
     private String userName;
+    private String location;
 
     @Builder
-    public UserUpdateRequestDto(String userName) {
+    public UserUpdateRequestDto(String userName, String location) {
         this.userName = userName;
+        this.location = location;
     }
 }
