3237a7ecd9b591c3adb3f2cf95a5c14deaf7740c
zestbear <zestbear90@gmail.com>
2023-11-24 04:27:37 +0900

    병원 이름 검색 기능 추가

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/hospital/service/HospitalService.java
@@ -0,0 +1,28 @@
+package com.personal.doctor.CapstoneDesign.hospital.service;
+
+import com.personal.doctor.CapstoneDesign.hospital.controller.dto.HospitalListResponseDto;
+import com.personal.doctor.CapstoneDesign.hospital.controller.dto.HospitalResponseDto;
+import com.personal.doctor.CapstoneDesign.hospital.domain.HospitalRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class HospitalService {
+
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    @Transactional
+    public List<HospitalListResponseDto> findHospitalType(String type) {
+        return hospitalRepository.findHospitalsByName(type).stream()
+                .map(HospitalListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+}
