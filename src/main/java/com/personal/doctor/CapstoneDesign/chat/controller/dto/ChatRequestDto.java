d4c6dd0e248fc61fb8b28bfb931ffa79aefd59f8
zestbear <zestbear90@gmail.com>
2023-11-25 03:39:46 +0900

    기본 생성자 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/chat/controller/dto/ChatRequestDto.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/chat/controller/dto/ChatRequestDto.java
@@ -3,7 +3,9 @@ package com.personal.doctor.CapstoneDesign.chat.controller.dto;
 import com.personal.doctor.CapstoneDesign.chat.domain.Chat;
 import com.personal.doctor.CapstoneDesign.user.domain.Users;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 public class ChatRequestDto {
 
