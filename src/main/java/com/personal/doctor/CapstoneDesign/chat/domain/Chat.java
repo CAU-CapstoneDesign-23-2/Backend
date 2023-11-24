8f9526c41c1718fac1af0fc97f13d5e37b00792b
zestbear <zestbear90@gmail.com>
2023-11-25 01:17:58 +0900

    생성형 AI 채팅 기록 저장을 위한 chat Entity 생성

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/chat/domain/Chat.java
@@ -0,0 +1,40 @@
+package com.personal.doctor.CapstoneDesign.chat.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.personal.doctor.CapstoneDesign.user.domain.Users;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long type;
+
+    @Column
+    private String content;
+
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "Users_id")
+    private Users users;
+
+    @Builder
+    public void chat(Long type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+}
