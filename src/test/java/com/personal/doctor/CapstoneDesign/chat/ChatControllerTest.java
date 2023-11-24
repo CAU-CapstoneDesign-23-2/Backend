677d2ac520f612f1a85ba567b76a71b77a6370b6
zestbear <zestbear90@gmail.com>
2023-11-25 03:41:11 +0900

    채팅 반환 POST 테스트

--- src/test/java/com/personal/doctor/CapstoneDesign/chat/ChatControllerTest.java
+++ src/test/java/com/personal/doctor/CapstoneDesign/chat/ChatControllerTest.java
@@ -1,6 +1,9 @@
 package com.personal.doctor.CapstoneDesign.chat;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personal.doctor.CapstoneDesign.chat.controller.dto.ChatRequestDto;
+import com.personal.doctor.CapstoneDesign.chat.controller.dto.ChatSaveRequestDto;
 import com.personal.doctor.CapstoneDesign.chat.domain.Chat;
 import com.personal.doctor.CapstoneDesign.chat.domain.ChatRepository;
 import com.personal.doctor.CapstoneDesign.chat.service.ChatService;
@@ -16,10 +19,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -85,6 +90,37 @@ public class ChatControllerTest {
 
         assertEquals(0L, chat.getType());
         assertEquals("채팅 저장 api 테스트", chat.getContent());
+    }
+
+    @Test
+    public void 채팅_반환() throws Exception {
+
+        ChatSaveRequestDto requestDto1 = ChatSaveRequestDto.builder()
+                .type(0L)
+                .content("40세 남자인데 무릎이 너무 아파. 어떻게 해야할까?")
+                .build();
+        chatService.save(userId, requestDto1);
+        ChatSaveRequestDto requestDto2 = ChatSaveRequestDto.builder()
+                .type(0L)
+                .content("72 여자인데 배가 너무 아파. 어떻게 해야할까?")
+                .build();
+        chatService.save(userId, requestDto2);
+
+        MvcResult mvcResult = mockMvc.perform(
+                        post("/userChats/" + userId)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String jsonResponse = mvcResult.getResponse().getContentAsString();
+        List<ChatRequestDto> chats = new ObjectMapper().readValue(jsonResponse, new TypeReference<List<ChatRequestDto>>(){});
+
+        assertEquals(2, chats.size());
+        assertEquals(0L, chats.get(0).getType());
+        assertEquals("40세 남자인데 무릎이 너무 아파. 어떻게 해야할까?", chats.get(0).getContent());
+        assertEquals(0L, chats.get(1).getType());
+        assertEquals("72 여자인데 배가 너무 아파. 어떻게 해야할까?", chats.get(1).getContent());
     }
 
 }
