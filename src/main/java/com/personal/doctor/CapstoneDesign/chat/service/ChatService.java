1fe914059401013a025211e3612f7b3cf6c1db79
zestbear <zestbear90@gmail.com>
2023-11-25 02:30:45 +0900

    채팅 저장 기능 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/chat/service/ChatService.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/chat/service/ChatService.java
@@ -3,23 +3,30 @@ package com.personal.doctor.CapstoneDesign.chat.service;
 import com.personal.doctor.CapstoneDesign.chat.controller.dto.ChatSaveRequestDto;
 import com.personal.doctor.CapstoneDesign.chat.domain.Chat;
 import com.personal.doctor.CapstoneDesign.chat.domain.ChatRepository;
+import com.personal.doctor.CapstoneDesign.user.domain.Users;
+import com.personal.doctor.CapstoneDesign.user.domain.UsersRepository;
+import com.personal.doctor.CapstoneDesign.util.exceptions.UserNotExistException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class ChatService {
 
+    private final UsersRepository usersRepository;
     private final ChatRepository chatRepository;
 
-    public ChatService(ChatRepository chatRepository) {
+    public ChatService(UsersRepository usersRepository, ChatRepository chatRepository) {
+        this.usersRepository = usersRepository;
         this.chatRepository = chatRepository;
     }
 
     @Transactional
-    public Long save(ChatSaveRequestDto chatSaveRequestDto) {
+    public Long save(Long userId, ChatSaveRequestDto chatSaveRequestDto) {
+        Users users = usersRepository.findById(userId)
+                .orElseThrow(() -> new UserNotExistException("사용자가 존재하지 않습니다."));
         Chat chat = chatSaveRequestDto.toEntity();
+
+        users.addChats(chat);
         chatRepository.save(chat);
 
         return chat.getId();
