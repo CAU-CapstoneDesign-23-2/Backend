e23ca593d3637a9b1cf207e9a57332574d5d2eb4
zestbear <zestbear90@gmail.com>
2023-11-25 01:18:46 +0900

    chat Repository 생성

---
+++ src/main/java/com/personal/doctor/CapstoneDesign/chat/domain/ChatRepository.java
@@ -0,0 +1,13 @@
+package com.personal.doctor.CapstoneDesign.chat.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+
+
+}
