3edd74a200a4b8c6afbdd2e8723b94193ab95d60
zestbear <zestbear90@gmail.com>
2023-11-26 15:58:37 +0900

    불필요한 nullable validation 삭제

--- src/main/java/com/personal/doctor/CapstoneDesign/hospital/domain/Hospital.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/hospital/domain/Hospital.java
@@ -14,25 +14,25 @@ public class Hospital {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column
     private String name;        // 병원 이름
 
-    @Column(nullable = false)
+    @Column
     private String code;        // 종별코드명 (Ex. 상급종합, 정신병원, 요양병원, 의원, 보건지소, 보건진료소, 치과병원, 치과의원, 한방병원, 한의원)
 
-    @Column(nullable = false)
+    @Column
     private String city;        // 시도
 
-    @Column(nullable = false)
+    @Column
     private String district;    // 시군구
 
     @Column
     private String town;        // 읍면동
 
-    @Column(nullable = false)
+    @Column
     private String postalCode;  // 우편번호
 
-    @Column(nullable = false)
+    @Column
     private String address;     // 주소
 
     @Column
