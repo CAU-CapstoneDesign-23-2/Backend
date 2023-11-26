151e95fed678ef3435a5ec3064d91a3862796430
zestbear <zestbear90@gmail.com>
2023-11-26 23:38:51 +0900

    병원 controller 주석 추가

--- src/main/java/com/personal/doctor/CapstoneDesign/hospital/controller/HospitalController.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/hospital/controller/HospitalController.java
@@ -16,11 +16,13 @@ public class HospitalController {
         this.hospitalService = hospitalService;
     }
 
+    // 이름에 있는 병원 종류로 병원 검색 Ex. ~한의원, ~내과, ~소아과
     @GetMapping(value = "/hospital/name", produces = "application/json;charset=UTF-8")
     public List<HospitalListResponseDto> withName(@RequestParam("type") String type) {
         return hospitalService.findHospitalType(type);
     }
 
+    // 사용자가 입력한 주소에 있는 병원들 반환
     @GetMapping(value = "/hospital/address", produces = "application/json;charset=UTF-8")
     public List<HospitalListResponseDto> withAddress(@RequestBody HospitalResponseDto responseDto) {
         return hospitalService.findHospitalAddress(responseDto);
