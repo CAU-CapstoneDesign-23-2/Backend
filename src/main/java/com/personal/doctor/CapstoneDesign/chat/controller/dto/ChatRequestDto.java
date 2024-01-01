a7cd921faead9e44162c50747e99f5d64d863408
zestbear <zestbear90@gmail.com>
2024-01-01 16:32:58 +0900

    refactor: 채팅 반환 DTO에서 순환 참조를 해결하기 위해 user 삭제

--- src/main/java/com/personal/doctor/CapstoneDesign/chat/controller/dto/ChatRequestDto.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/chat/controller/dto/ChatRequestDto.java
@@ -1,7 +1,6 @@
 package com.personal.doctor.CapstoneDesign.chat.controller.dto;
 
 import com.personal.doctor.CapstoneDesign.chat.domain.Chat;
-import com.personal.doctor.CapstoneDesign.user.domain.Users;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,13 +11,11 @@ public class ChatRequestDto {
     private Long id;
     private String requestText;
     private String responseText;
-    private Users users;
 
     public ChatRequestDto(Chat entity) {
         id = entity.getId();
         requestText = entity.getRequestText();
         responseText = entity.getResponseText();
-        users = entity.getUsers();
     }
 
 }
