40f22e776629766b6cb583ce673b867c09c0fc43
zestbear <zestbear90@gmail.com>
2024-01-18 15:17:59 +0900

    :recycle: refactor: 한글 인코딩 확인을 위해 GET restful api를 POST로 변경

--- src/main/java/com/personal/doctor/CapstoneDesign/hospital/controller/HospitalController.java
+++ src/main/java/com/personal/doctor/CapstoneDesign/hospital/controller/HospitalController.java
@@ -17,7 +17,8 @@ public class HospitalController {
     }
 
     // 이름에 있는 병원 종류로 병원 검색 Ex. ~한의원, ~내과, ~소아과
-    @GetMapping(value = "/hospital/name/{userID}", produces = "application/json;charset=UTF-8")
+//    @GetMapping(value = "/hospital/name/{userID}", produces = "application/json;charset=UTF-8")
+    @PostMapping("/hospital/name/{userID}")
     public List<HospitalListResponseDto> withName(@PathVariable Long userID,
                                                   @RequestParam("type") String type) {
         return hospitalService.findHospitalType(userID, type);
@@ -25,14 +26,16 @@ public class HospitalController {
 
     // 사용자가 입력한 주소에 있는 병원들 반환
     @Cacheable(value = "userHospitals", key = "#userID + '_' + 'Hospitals'")
-    @GetMapping(value = "/hospital/address/{userID}", produces = "application/json;charset=UTF-8")
+//    @GetMapping(value = "/hospital/address/{userID}", produces = "application/json;charset=UTF-8")
+    @PostMapping("/hospital/address/{userID")
     public List<HospitalListResponseDto> withAddress(@PathVariable Long userID) {
         return hospitalService.findHospitalAddress(userID);
     }
 
     // 메인 화면에 노출할 사용자 거주지 주변 병원 한 개 반환
     @Cacheable(value = "userHospital", key = "#userID + '_' + 'Hospital'")
-    @GetMapping(value = "/hospital/main/{userID}", produces = "application/json;charset=UTF-8")
+//    @GetMapping(value = "/hospital/main/{userID}", produces = "application/json;charset=UTF-8")
+    @PostMapping("/hospital/main/{userID")
     public HospitalListResponseDto onMain(@PathVariable Long userID) {
         HospitalListResponseDto result = hospitalService.findOneHospitalByAddress(userID);
         System.out.println("Cache Not Used: " + result);
